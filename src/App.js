02e952a6cf2dcb8a3a069ea2b5507b56f51d0c4a
Greta Ra <ggretute88@gmail.com>
2023-03-29 14:03:03 +0200

    Change class components to functional components in 'General component'

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import { Component } from "react";
 import "./style/App.css";
 import General from "./components/General";
 import Education from "./components/Education";
 import Experience from "./components/Experience";
 
-class App extends Component {
-	render() {
-		return (
-			<div className="cv-container">
-				<div className="cv-content">
-					<h1>Create your CV</h1>
-					<General />
-					<Education />
-					<Experience />
-				</div>
+const App = () => {
+	return (
+		<div className="cv-container">
+			<div className="cv-content">
+				<h1>Create your CV</h1>
+				<General />
+				<Education />
+				<Experience />
 			</div>
-		);
-	}
+		</div>
+	);
 }
 
 export default App;
--- src/components/General.js
+++ src/components/General.js
@@ -1,177 +1,172 @@
-import { Component } from "react";
+import { useState } from "react";
 import "./../style/form.css";
 
-class General extends Component {
-	constructor(props) {
-		super(props);
+const General = () => {
+	const [status, setStatus] = useState("isNew");
 
-		this.state = {
-			status: "isNew",
-		};
-		this.handleChange = this.handleChange.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
-		this.handleValueEdit = this.handleValueEdit.bind(this);
-	}
+	const initialValues = {
+		firstName: "",
+		lastName: "",
+		email: "",
+		phoneNumber: "",
+	};
 
-	handleChange(event) {
-		const target = event.target;
-		const name = target.name;
-		const value = target.value;
+	const [values, setValues] = useState(initialValues);
 
-		this.setState({
+	const handleChange = (event) => {
+		const { name, value } = event.target;
+
+		setValues({
+			...values,
 			[name]: value,
 		});
-	}
+	};
 
-	handleSubmit(event) {
-		this.setState({ status: "isSubmitted" });
+	const handleSubmit = (event) => {
+		setStatus("isSubmitted");
 		event.preventDefault();
-	}
+	};
 
-	handleValueEdit(event) {
-		this.setState({ status: "isEdit" });
+	const handleValueEdit = (event) => {
+		setStatus("isEdit");
 		event.preventDefault();
+	};
+
+	if (status === "isNew") {
+		return (
+			<div>
+				<h2 className="form-header">Personal information</h2>
+				<form onSubmit={handleSubmit}>
+					<div className="form-layout">
+						<div className="form-column">
+							<label>First name:</label>
+							<input
+								value={values.firstName}
+								onChange={handleChange}
+								type="text"
+								name="firstName"
+								placeholder="Ada"
+								required
+							></input>
+							<label>Last name:</label>
+							<input
+								value={values.lastName}
+								onChange={handleChange}
+								type="text"
+								name="lastName"
+								placeholder="Lovelace"
+								required
+							></input>
+						</div>
+						<div className="form-column">
+							<label>Email:</label>
+							<input
+								value={values.email}
+								onChange={handleChange}
+								type="email"
+								name="email"
+								placeholder="lovelace@pigeonmail.com"
+								required
+							></input>
+							<label>Phone number:</label>
+							<input
+								value={values.phoneNumber}
+								onChange={handleChange}
+								type="text"
+								name="phoneNumber"
+								placeholder="123123"
+								required
+							></input>
+						</div>
+					</div>
+					<button className="form-button" type="submit">
+						Save
+					</button>
+				</form>
+			</div>
+		);
 	}
 
-	render() {
-		const { status, value } = this.state;
-
-		if (status === "isNew") {
-			return (
-				<div>
-					<h2 className="form-header">Personal information</h2>
-					<form onSubmit={this.handleSubmit}>
-						<div className="form-layout">
-							<div className="form-column">
-								<label>First name:</label>
-								<input
-									value={value}
-									onChange={this.handleChange}
-									type="text"
-									name="firstName"
-									placeholder="Ada"
-									required
-								></input>
-								<label>Last name:</label>
-								<input
-									value={value}
-									onChange={this.handleChange}
-									type="text"
-									name="lastName"
-									placeholder="Lovelace"
-									required
-								></input>
-							</div>
-							<div className="form-column">
-								<label>Email:</label>
-								<input
-									value={value}
-									onChange={this.handleChange}
-									type="email"
-									name="email"
-									placeholder="lovelace@pigeonmail.com"
-									required
-								></input>
-								<label>Phone number:</label>
-								<input
-									value={value}
-									onChange={this.handleChange}
-									type="text"
-									name="phoneNumber"
-									placeholder="123123"
-									required
-								></input>
-							</div>
+	if (status === "isEdit") {
+		return (
+			<div>
+				<h2 className="form-header">Personal information</h2>
+				<form onSubmit={handleSubmit}>
+					<div className="form-layout">
+						<div className="form-column">
+							<label>First name:</label>
+							<input
+								value={values.firstName}
+								onChange={handleChange}
+								type="text"
+								name="firstName"
+								placeholder="Ada"
+								required
+							></input>
+							<label>Last name:</label>
+							<input
+								value={values.lastName}
+								onChange={handleChange}
+								type="text"
+								name="lastName"
+								placeholder="Lovelace"
+								required
+							></input>
 						</div>
-						<button className="form-button" type="submit">
-							Save
-						</button>
-					</form>
-				</div>
-			);
-		}
-
-		if (status === "isEdit") {
-			return (
-				<div>
-					<h2 className="form-header">Personal information</h2>
-					<form onSubmit={this.handleSubmit}>
-						<div className="form-layout">
-							<div className="form-column">
-								<label>First name:</label>
-								<input
-									value={this.state.firstName}
-									onChange={this.handleChange}
-									type="text"
-									name="firstName"
-									placeholder="Ada"
-									required
-								></input>
-								<label>Last name:</label>
-								<input
-									value={this.state.lastName}
-									onChange={this.handleChange}
-									type="text"
-									name="lastName"
-									placeholder="Lovelace"
-									required
-								></input>
-							</div>
-							<div className="form-column">
-								<label>Email:</label>
-								<input
-									value={this.state.email}
-									onChange={this.handleChange}
-									type="email"
-									name="email"
-									placeholder="lovelace@pigeonmail.com"
-									required
-								></input>
-								<label>Phone number:</label>
-								<input
-									value={this.state.phoneNumber}
-									onChange={this.handleChange}
-									type="text"
-									name="phoneNumber"
-									placeholder="123123"
-								></input>
-							</div>
+						<div className="form-column">
+							<label>Email:</label>
+							<input
+								value={values.email}
+								onChange={handleChange}
+								type="email"
+								name="email"
+								placeholder="lovelace@pigeonmail.com"
+								required
+							></input>
+							<label>Phone number:</label>
+							<input
+								value={values.phoneNumber}
+								onChange={handleChange}
+								type="text"
+								name="phoneNumber"
+								placeholder="123123"
+							></input>
 						</div>
-						<button className="form-button" type="submit">
-							Save
-						</button>
-					</form>
-				</div>
-			);
-		}
-
-		if (status === "isSubmitted") {
-			return (
-				<div>
-					<h2 className="form-header">Personal information</h2>
-					<form onSubmit={this.handleValueEdit}>
-						<div className="form-display">
-							<div className="form-column-display">
-								<span className="name-display">First name:</span>
-								{this.state.firstName}
-								<span className="name-display">Last name:</span>
-								{this.state.lastName}
-							</div>
-							<div className="form-column-display">
-								<span className="name-display">Email:</span>
-								{this.state.email}
-								<span className="name-display">Phone number:</span>
-								{this.state.phoneNumber}
-							</div>
-						</div>
-						<button className="form-button" type="submit">
-							Edit
-						</button>
-					</form>
-				</div>
-			);
-		}
+					</div>
+					<button className="form-button" type="submit">
+						Save
+					</button>
+				</form>
+			</div>
+		);
 	}
-}
+
+	if (status === "isSubmitted") {
+		return (
+			<div>
+				<h2 className="form-header">Personal information</h2>
+				<form onSubmit={handleValueEdit}>
+					<div className="form-display">
+						<div className="form-column-display">
+							<span className="name-display">First name:</span>
+							{values.firstName}
+							<span className="name-display">Last name:</span>
+							{values.lastName}
+						</div>
+						<div className="form-column-display">
+							<span className="name-display">Email:</span>
+							{values.email}
+							<span className="name-display">Phone number:</span>
+							{values.phoneNumber}
+						</div>
+					</div>
+					<button className="form-button" type="submit">
+						Edit
+					</button>
+				</form>
+			</div>
+		);
+	}
+};
 
 export default General;
